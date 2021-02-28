a749b1d5939cad53c7d6241b16e6fe5751509af3
janvishah21 <janvimshah21@gmail.com>
2021-02-28 23:06:50 +0530

    add event utility added

--- event/models.py
+++ event/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=100)
-    date = models.DateField()
+    date = models.DateField('date')
     time = models.TimeField()
     location = models.CharField(max_length=30)
     image = models.ImageField(upload_to="event/event-img", blank=True)
--- event/urls.py
+++ event/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	path('create-event', views.create, name='create')
+	path('create-event', views.create, name='create'),
+	path('liked', views.liked, name='liked')
 ]
